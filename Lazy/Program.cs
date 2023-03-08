313c353f0118fa0f68059d8f7e05cb89001ddd3e
arslanmurat06 <mrtarsln06@gmail.com>
2023-03-08 13:35:49 +0300

    desc added

--- Lazy/Program.cs
+++ Lazy/Program.cs
@@ -7,6 +7,8 @@ var employees = await employeeRepository.GetEmployees();
 
 employees.ForEach(e => Console.WriteLine(e.Name));
 
+
+//This time Create Employee method will not be executed so only the employees created before will be returned
 var employees2 = await employeeRepository.GetEmployees();
 
 
